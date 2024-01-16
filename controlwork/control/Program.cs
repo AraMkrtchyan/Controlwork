2abc2ab69c15499cfae90213dfd57fed65cc68cf
Ara Mkrtchyan <mkrtchyanara22@gmail.com>
2024-01-16 21:05:06 +0400

    задаем рандомний масив

---
+++ controlwork/control/Program.cs
@@ -0,0 +1,11 @@
+﻿void Array(int[] numbers)
+{
+    int length = numbers.Length;
+    int index = 0;
+    while (index < length)
+    {
+        numbers[index] = new Random().Next(1, 10);
+        index++;
+    }
+
+}
